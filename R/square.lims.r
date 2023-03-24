33d1cb9dadcddffc582bf79dc2ad0e60a832a449
Brian Macdonald <bmacgtpm@gmail.com>
2023-03-24 13:11:33 -0400

    add documentation

--- R/square.lims.r
+++ R/square.lims.r
@@ -1,9 +1,15 @@
+#' Given data, create square limits based on that data
+#'
+#' Create square limits for scatter plots, meaning x and y have the same limits.
+#' @param data Data to base the limits on
+#' @param symmetric If TRUE, then the limits will be symmetric about the origin.  Default is FALSE.
+#' @export
 square.lims = function(data=NULL, symmetric=F){
-  
+
   ## find min and max
-  lims = c(min(data, na.rm=T), 
+  lims = c(min(data, na.rm=T),
            max(data, na.rm=T))
-  
+
   if(symmetric==T){
     abs.max = max(abs(lims))
     lims = c(-abs.max, abs.max)
